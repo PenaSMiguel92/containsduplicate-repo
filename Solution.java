c8115238eed5748b06adbfb5e8c39666a716c859
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-22 16:19:18 -0700

    final answer

--- Solution.java
+++ Solution.java
@@ -1,15 +1,12 @@
+import java.util.Set;
 import java.util.HashSet;
 
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-
+        Set<Integer> set = new HashSet<>();
         for (int num : nums) {
-            if (!set.contains(num)) {
-                set.add(num);
-            } else {
+            if (!set.add(num))
                 return true;
-            }
         }
         return false;
     }

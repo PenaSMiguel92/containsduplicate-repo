549c7d368cd8dff6f1d703988a03705eb8b21617
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-22 16:07:07 -0700

    main method implemented

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 public class Main { 
-public static void main(String[] args) {
-}
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.containsDuplicate(new int[] { 1, 2, 3, 1 }));
+        System.out.println(sol.containsDuplicate(new int[] { 1, 2, 3, 4 }));
+        System.out.println(sol.containsDuplicate(new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }));
+        
+    }
 }
